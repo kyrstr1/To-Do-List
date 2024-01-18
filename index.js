f98513b09a9be7741860499db3da8b720fab5fab
Kayra SATIR <141830248+kyrstr1@users.noreply.github.com>
2024-01-19 02:21:53 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+function addTask() {
+    var taskInput = document.getElementById("new-task");
+    var taskText = taskInput.value.trim();
+
+    if (taskText !== "") {
+        var tasksContainer = document.getElementById("tasks");
+        var newTaskItem = document.createElement("li");
+        newTaskItem.innerHTML = `
+            <span>${taskText}</span>
+            <button onclick="removeTask(this)">Remove</button>
+        `;
+        tasksContainer.appendChild(newTaskItem);
+        taskInput.value = "";
+    }
+}
+
+function removeTask(button) {
+    var taskItem = button.parentNode;
+    taskItem.parentNode.removeChild(taskItem);
+}
